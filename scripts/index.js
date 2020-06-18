54d91960e1114b215baf460f4578ea39a252ce1a
Alextpprt <63902550+Alextpprt@users.noreply.github.com>
2020-06-18 18:27:32 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,28 @@
-let buttonOpen = document.querySelector('.profile__edit-button');
+let openButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let buttonClose = popup.querySelector('.popup__close-button');
+let closeButton = popup.querySelector('.popup__close-button');
 let inputName = popup.querySelector('.popup__input_type_name');
 let inputJob = popup.querySelector('.popup__input_type_activity');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__activity');
-let buttonSave = popup.querySelector('.popup__info');
+let saveButton = popup.querySelector('.popup__info');
 
-function popupOpen() {
-    popup.classList.add('popup_opened');
-    inputName.value = profileName.textContent;;
-    inputJob.value = profileJob.textContent;
-}
-
-function popupClose() {
-    popup.classList.remove('popup_opened');
-}
+function popupToggle () {
+    if (popup.classList.toggle('popup_opened')) {
+        inputName.value = profileName.textContent;
+        inputJob.value = profileJob.textContent;
+      }
+  }
+  
+openButton.addEventListener('click', popupToggle);
+closeButton.addEventListener('click', popupToggle);
 
 function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    inputName.getAttribute('value');
-    inputJob.getAttribute('value');
-    profileName.textContent = inputName.value;
-    profileJob.textContent = inputJob.value; 
-    popupClose()
-}
+        evt.preventDefault();
+        profileName.textContent = inputName.value;
+        profileJob.textContent = inputJob.value; 
+        popupToggle ();
+      }
 
-buttonOpen.addEventListener('click', popupOpen);
-buttonClose.addEventListener('click', popupClose);
-buttonSave.addEventListener('submit', formSubmitHandler);
+saveButton.addEventListener('submit', formSubmitHandler);
+
